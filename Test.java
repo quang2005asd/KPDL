932313ed9bc60d51641e839c6ff7ede4dded16cd
quang2005asd <quangcokhi2005@gmail.com>
2025-12-05 22:13:29 +0700

    test java

--- Test.java
+++ Test.java
@@ -1,8 +1,41 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            i += i;
-            System.out.println("tổng của i khi i = 5 là: " + i);
+        int sum = 0;
+        for (int i = 0; i <= 5; i++) {
+            sum += i;
         }
+        System.out.println("tổng của i khi i = 5 là: " + sum);
+        tinhtoan();
     }
+
+    public static void tinhtoan(){
+        Scanner tt = new Scanner(System.in); 
+        System.out.println("nhập số a: ");
+        double a = tt.nextDouble();
+        System.out.println("nhập số b: ");
+        double b = tt.nextDouble();
+
+        System.out.println("Chọn phép toán mà bạn muốn thực hiện(+,-,*,/): ");
+        char pheptoan = tt.next().charAt(0);
+        if(pheptoan == '+'){
+            System.out.println("kết quả của phép cộng là: " + (a + b));
+        } else if(pheptoan == '-'){
+            System.out.println("kết quả của phép trừ là: " + (a - b));
+        } else if(pheptoan == '*'){
+            System.out.println("kết quả của phép nhân là: " + (a * b));
+        } else if(pheptoan == '/'){
+            if(b != 0){
+                System.out.println("kết quả của phép chia là: " + (a / b));
+            } else {
+                System.out.println("Lỗi: Không thể chia cho 0.");
+            }
+        } else {
+            System.out.println("Phép toán không hợp lệ.");
+        }
+        tt.close();
 }
+}
+
+
